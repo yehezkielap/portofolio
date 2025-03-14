9d54d9496c740d4180a93dd6198360dd18f22166
yehezkielap <yehezkieladityapratama24@gmail.com>
2025-03-14 07:20:53 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container : {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors :{
+        biru : "#2563eb",
+        hitam : "#020617",
+      },
+    },
+  },
+  plugins: [],
+}
